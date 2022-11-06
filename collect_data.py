62d002fc28d72bb2b9debeda449a244933d9a1ef
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2022-11-06 12:35:43 -0800

    works :)

--- collect_data.py
+++ collect_data.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
-from mpu9250_jmdev.registers import *
-from mpu9250_jmdev.mpu_9250 import MPU9250
+import board
+import adafruit_mpu6050
 from gpiozero import LED, Button
 
 def run():
@@ -10,25 +10,14 @@ def run():
     write_led = LED(8)
     start_stop_btn = Button(23)
 
-    # setup IMU
-    mpu = MPU9250(
-        address_ak=AK8963_ADDRESS, 
-        address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
-        address_mpu_slave=None, 
-        bus=1,
-        gfs=GFS_1000, 
-        afs=AFS_8G, 
-        mfs=AK8963_BIT_16, 
-        mode=AK8963_MODE_C100HZ
-    )
-    mpu.configure() # Apply the settings to the registers.
-
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    mpu = adafruit_mpu6050.MPU6050(i2c)
     record_data = False
 
     # setup CSV file
     file = open('data.csv', 'a')
     file.truncate(0) # clear file
-    file.write("time,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,magno_x,magno_y,magno_z,temp\n")
+    file.write("time,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,temp\n")
 
     start_time = datetime.now()
     while True:
@@ -39,10 +28,9 @@ def run():
             write_led.on()
             record_led.on()
 
-            accel = mpu.readAccelerometerMaster()
-            gyro = mpu.readGyroscopeMaster()
-            magno = mpu.readMagnetometerMaster()
-            temp = mpu.readTemperatureMaster()
+            accel = mpu.acceleration
+            gyro = mpu.gyro
+            temp = mpu.temperature
 
             time.sleep(0.125)
             write_led.off()
@@ -55,7 +43,6 @@ def run():
 
             csvline += f",{accel[0]},{accel[1]},{accel[2]}"
             csvline += f",{gyro[0]},{gyro[1]},{gyro[2]}"
-            csvline += f",{magno[0]},{magno[1]},{magno[2]}"
             csvline += f",{temp}\n"
             print(csvline,end='')
             file.write(csvline)
